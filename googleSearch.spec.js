1dae58b6f67df2c6648999e0d56d74f62e6c798b
Devendra Gawade <devendragawade@Devendras-MacBook-Air.local>
2025-09-25 17:06:41 +0530

    Add fresh BrowserStack DuckDuckGo test

--- googleSearch.spec.js
+++ googleSearch.spec.js
@@ -1,20 +1,25 @@
 const { chromium } = require('playwright');
 
 (async () => {
-    const browser = await chromium.launch({ headless: false });
+    const browser = await chromium.connect({
+        wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(JSON.stringify({
+            'browser': 'chrome',
+            'browser_version': 'latest',
+            'os': 'osx',
+            'os_version': 'ventura',
+            'name': 'DuckDuckGo Test',
+            'build': 'Playwright Jenkins Build',
+            'browserstack.username': 'devendragawade_a7XBUS',
+            'browserstack.accessKey': 'zsVnCSszQ9C8JX7FnZKA'
+        }))}`
+    });
+
     const page = await browser.newPage();
     await page.goto('https://duckduckgo.com/');
-
-    // Wait for the search box
     await page.waitForSelector('input[name="q"]', { timeout: 60000 });
-
-    // Type search query
     await page.fill('input[name="q"]', 'Hello World');
     await page.press('input[name="q"]', 'Enter');
-
-    // Wait to see results
     await page.waitForTimeout(5000);
-
     await browser.close();
 })();
 
